f17d917fd2e1c82fdf661572f893e1acd4b973bc
klaus <kgf@omniversum.de>
2019-03-28 16:32:58 +0100

    Fixed autoplay, maybe

--- R/item_page.R
+++ R/item_page.R
@@ -14,6 +14,12 @@ media_js <- list(
   media_not_played = "var media_played = false;",
   media_played = "media_played = true;",
   play_media = "document.getElementById('media').play();",
+  show_media   = paste0("if (!media_played) ",
+                        "{document.getElementById('media')",
+                        ".style.visibility='inherit'};"),
+  hide_media   = paste0("if (media_played) ",
+                          "{document.getElementById('media')",
+                          ".style.visibility='hidden'};"),
   show_media_btn = paste0("if (!media_played) ",
                           "{document.getElementById('btn_play_media')",
                           ".style.visibility='inherit'};"),
@@ -22,10 +28,10 @@ media_js <- list(
   show_responses = "document.getElementById('response_ui').style.visibility = 'inherit';"
 )
 
-media_mobile_play_button <- shiny::tags$p(
+media_mobile_play_button <- shiny::tags$button(
 #  shiny::tags$strong(psychTestR::i18n("CLICK_HERE_TO_PLAY")),
   id = "btn_play_media",
-  style = "visibility: hidden",
+  style = "visibility: visible;height: 50px",
   onclick = media_js$play_media
 )
 
@@ -53,13 +59,14 @@ get_audio_ui <- function(url,
     loop = if (loop) "loop",
     oncanplaythrough = media_js$show_media_btn,
     onplay = paste0(media_js$media_played, media_js$hide_media_btn),
-    onended = if (wait) media_js$show_responses else "null"
+    onended = if (wait) paste0(media_js$show_responses, media_js$hide_media) else "null"
   )
   shiny::tags$div(audio, media_mobile_play_button)
 }
 
 get_audio_element <- function(url,
                               type = tools::file_ext(url),
+                              wait = F,
                               autoplay = FALSE,
                               width = 200,
                               height = 50,
@@ -69,15 +76,19 @@ get_audio_element <- function(url,
             purrr::is_scalar_character(type)
             )
   src    <- shiny::tags$source(src = url, type = paste0("audio/", type))
+  script <- shiny::tags$script(shiny::HTML(media_js$media_not_played))
   audio  <- shiny::tags$audio(
     src,
+    script,
     id = id,
     preload = "auto",
     controls = "controls",
-    controlslist = "nodownload",
+    controlslist = "nodownload noremoteplayback",
     autoplay = if(autoplay) "autoplay",
     width = width,
-    height = height
+    height = height,
+    onplay = paste0(media_js$media_played, media_js$hide_media),
+    onended = if (wait) paste0(media_js$show_responses, media_js$hide_media) else "null"
   )
   audio
 }
@@ -93,7 +104,8 @@ audio_NAFC_page_with_img <- function(label,
                                      on_complete = NULL,
                                      admin_ui = NULL) {
   stopifnot(purrr::is_scalar_character(label))
-  audio_ui <- get_audio_ui(audio_url, wait = T, loop = F)
+  #audio_ui <- get_audio_ui(audio_url, wait = T, loop = F)
+  audio_ui <- get_audio_element(audio_url, autoplay = T, wait = T, width = 50)
   style <- NULL
   if(hide_response_ui) style <- "visibility:hidden"
   ui <- shiny::div(
