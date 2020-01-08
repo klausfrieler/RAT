389e1a1ee7bf07c44a5d74c69c523f4d110e3394
klaus <kgf@omniversum.de>
2020-01-08 17:36:25 +0100

    Text fixes

--- data_raw/RAT_dict.R
+++ data_raw/RAT_dict.R
@@ -2,4 +2,4 @@ RAT_dict_raw <- readRDS("data_raw/RAT_dict.RDS")
 names(RAT_dict_raw) <- c("key", "DE", "EN")
 RAT_dict_raw <- RAT_dict_raw[,c("key", "EN", "DE")]
 RAT_dict <- psychTestR::i18n_dict$new(RAT_dict_raw)
-#usethis::use_data(RAT_dict, overwrite = TRUE)
+usethis::use_data(RAT_dict, overwrite = TRUE)
