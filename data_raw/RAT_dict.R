1ad974ce2553064d94a07a4549a2fbb30cf02723
klaus <kgf@omniversum.de>
2021-10-15 22:50:00 +0200

    RAT_dict as Excel

--- data_raw/RAT_dict.R
+++ data_raw/RAT_dict.R
@@ -1,5 +1,6 @@
-RAT_dict_raw <- readRDS("data_raw/RAT_dict.RDS")
-names(RAT_dict_raw) <- c("key", "DE", "EN")
-RAT_dict_raw <- RAT_dict_raw[,c("key", "EN", "DE")]
+# RAT_dict_raw <- readRDS("data_raw/RAT_dict.RDS")
+# names(RAT_dict_raw) <- c("key", "DE", "EN")
+# RAT_dict_raw <- RAT_dict_raw[,c("key", "EN", "DE")]
+RAT_dict_raw <-readxl::read_xlsx("data_raw/RAT_dict.xlsx")
 RAT_dict <- psychTestR::i18n_dict$new(RAT_dict_raw)
 usethis::use_data(RAT_dict, overwrite = TRUE)
